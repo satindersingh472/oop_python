c6f539833c9249339bd0bd9ef529df9f9f80baa0
satinder <satindersingh472@gmail.com>
2023-02-08 23:00:07 -0700

    finished

--- app.py
+++ app.py
@@ -10,6 +10,14 @@ class Dog(Animal):
     def __init__(self,name):
         super().__init__(name,sound="woofff!!!")
 
+class Cat(Animal):
+    def __init__(self,name,sound):
+        super().__init__(name,sound)
+
 d1 = Dog("jango")
+c1 = Cat('billi','meow')
+
+c1.make_noise()
+
 
 d1.make_noise()
